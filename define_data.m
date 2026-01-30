2487475e0efc1c8b4b53a9f437b31e13b956fe03
Mihaela-Alexandra97 <mihaela-alexandra.barb-ciorbea@academic.tuiasi.ro>
2026-01-30 12:36:21 +0200

    define_data.m

--- define_data.m
+++ define_data.m
@@ -1,24 +1,28 @@
-function [env_bounds,numCities,numVehicles,numObstacles,vehicleSpeed,M,e,l]=define_data();
+function [env_bounds,numCities,numVehicles,numObstacles,vehicleSpeed,M,e,l] = define_data()
 
-env_bounds=[0 200 0 200];
+numCities     = 7;
+numVehicles   = 10;
+numObstacles  = 5;
+vehicleSpeed  = 10;
+M             = 1000;
+env_bounds    = [0 200 0 200];
 
-travelTimeRow = [0, 12.50, 10.69, 6.25, 11.26, 8.28, 11.27, 10.86, 10.01, 5.34, ...
-                  13.22, 7.88, 9.40, 14.05, 6.98, 12.11, 11.75, 10.56, 9.87, 8.44];
-numCities = 7;
+
+% Depot has zero travel time to itself
+travelTimeRow = zeros(1, numCities);
+
+%  random but bounded travel times for cities 2..numCities
+travelTimeRow(2:end) = 5 + 10*rand(1, numCities-1);   % [5,15] seconds
+
 e = zeros(numCities,1);
 l = zeros(numCities,1);
 
 for i = 2:numCities
-    e(i) = ceil(travelTimeRow(i)) + randi(4);   % e.g., arrival time from depot + buffer
+    e(i) = ceil(travelTimeRow(i)) + randi(4);
     l(i) = e(i) + randi([10, 20]);
-;                      
 end
 
-e(1) = 0;    % depot
-l(1) = 1e6;  % no upper bound (or Inf)
-numVehicles=10;
-numObstacles=5;
-vehicleSpeed=10;
-M=1000;
+e(1) = 0;      % depot
+l(1) = 1e6;    % no upper bound
 
 end
